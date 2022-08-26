ff05306fe5af9e8edb479095118b67e403f98e17
Aldo Lugo <aldo@aldomatic.com>
2022-08-26 23:08:44 +0000

    testing..

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 Example repo to build a docker image via actions and publish to github for hosting
 
 
-### KubeCTL commands
+##### KubeCTL commands
 
 Create new deployment 
 
-`kube ctl create deployment -f SOMEFILE.yaml`
+`kube ctl create deployment -f deploy.yaml`
+
+Rolling update will 
+
+`kubectl rollout restart deploy deploy.yaml`
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const os = require('os')
 
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
 const app = express()
 app.get('/', (req, res) => {
-        res.send(`Hi, Oliver from pod(V3):  ${os.hostname()}!`)
+        res.send(`Hi, Ashley from pod:  ${os.hostname()}!`)
 })
 
-const port = 3000
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
