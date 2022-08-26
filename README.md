d80069667364cf3090a3d126363573aa00472587
Aldo Lugo <aldo@aldomatic.com>
2022-08-26 22:56:29 +0000

    test

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # docker-image-host-github
 Example repo to build a docker image via actions and publish to github for hosting
+
+
+### KubeCTL commands
+
+Create new deployment 
+
+`kube ctl create deployment -f SOMEFILE.yaml`
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const os = require('os')
 
 const app = express()
 app.get('/', (req, res) => {
-        res.send(`Hi, Human from pod(V2):  ${os.hostname()}!`)
+        res.send(`Hi, Oliver from pod(V3):  ${os.hostname()}!`)
 })
 
 const port = 3000
